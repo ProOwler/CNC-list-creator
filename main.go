d21367af39b9434f06e64be76f497d4e2545a44b
ProOwler <proowler@gmail.com>
2024-05-12 16:36:16 +0700

    Немного причесал

--- main.go
+++ main.go
@@ -19,12 +19,37 @@ const (
 )
 
 func main() {
-	// myString := outputToOneString(getOutput())
 	// nameForListOfFiles := "list.xml"
 	nameForListOfFiles := "output.txt"
 
-	check(os.WriteFile(nameForListOfFiles, []byte(outputToOneString(getOutput())), 0666))
+	listOfFileFormats := []string{"xml", "XML", "mpr", "MPR"}
+	if len(listOfFileFormats) > 1 {
+	} // чтоб компилятор не ругался на неиспользование массива
+
+	check(os.WriteFile(nameForListOfFiles, []byte(getOutput()), 0666))
 	fmt.Println("Успешно!")
+}
+
+/*
+func recursiveWalkthrough(startPath string, outputFilename string, fileFormats []string, ) {
+	s
+}
+*/
+
+func getOutput() string {
+	absoluteFilepath, err := filepath.Abs(getMainStartupArg())
+	check(err)
+	return outputToOneString(getListOfDirAndFiles(absoluteFilepath))
+}
+
+func getMainStartupArg() string {
+	if len(os.Args) > 1 {
+		fmt.Println("Скормлена папка: " + os.Args[1])
+		return (os.Args[1])
+	} else {
+		fmt.Println("Вызван сам по себе: " + os.Args[0])
+		return os.Args[0]
+	}
 }
 
 func outputToOneString(strings []string) string {
@@ -36,22 +61,8 @@ func outputToOneString(strings []string) string {
 	return myString
 }
 
-func getOutput() []string {
-	absoluteFilepath, err := filepath.Abs(getAbsoluteFilepathFromStartupArgs())
-	check(err)
-	return getListOfDirAndFiles(absoluteFilepath)
-}
-
 func getListOfDirAndFiles(givenFilename string) []string {
-	myFileInfo, err1 := os.Stat(givenFilename)
-	check(err1)
-
-	mydirPath := givenFilename
-	if !myFileInfo.IsDir() {
-		mydirPath = filepath.Dir(givenFilename)
-	}
-
-	myFile, err1 := os.Open(mydirPath)
+	myFile, err1 := os.Open(getDirOfArg(givenFilename))
 	check(err1)
 
 	myList, err1 := myFile.Readdirnames(-1)
@@ -60,15 +71,15 @@ func getListOfDirAndFiles(givenFilename string) []string {
 	return myList
 }
 
-func getAbsoluteFilepathFromStartupArgs() string {
-	//return "D:\\Prowler\\projects\\CNC-list-creator\\proj\\.git\\"
-	if len(os.Args) > 1 {
-		fmt.Println("Скормлена папка: " + os.Args[1])
-		return (os.Args[1])
-	} else {
-		fmt.Println("Вызван сам по себе: " + os.Args[0])
-		return os.Args[0]
+func getDirOfArg(givenFilename string) string {
+	myFileInfo, err1 := os.Stat(givenFilename)
+	check(err1)
+
+	mydirPath := givenFilename
+	if !myFileInfo.IsDir() {
+		mydirPath = filepath.Dir(givenFilename)
 	}
+	return mydirPath
 }
 
 func check(e error) {
