eff190576d5c60d0378213583eee44843133e202
ProOwler <proowler@gmail.com>
2024-05-14 03:02:01 +0700

    Рекурсивный обход папок (начиная либо с указанной папки, либо с папки, содержащей исполняемый файл)
    В каждой папке с интересующими нас файлами создаётся файл-список в формате XML, созданный по специальному шаблону

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"path/filepath"
 	"slices"
+	"strings"
+	"unicode"
 )
 
 const (
@@ -35,108 +37,92 @@ const (
 */
 
 func main() {
-	// nameForListOfFiles := "list.xml"
-	nameForListOfFiles := "output.txt"
+	nameForListOfFiles := "list.xml"
+	// nameForListOfFiles := "output.txt"
 
-	// чтоб компилятор не ругался на неиспользование массива
-	listOfFileFormats := []string{"xml", "XML", "mpr", "MPR", "txt"}
-	if len(listOfFileFormats) > 1 {
-	}
+	listOfFileFormats := []string{"xml", "XML", "mpr", "MPR"}
 
 	recursiveWalkthrough(getStartDirPath(), nameForListOfFiles, listOfFileFormats)
 
 }
 
 func recursiveWalkthrough(startPath string, outputFilename string, fileFormats []string) {
-	fmt.Println("recursiveWalkthrough: " + startPath + " // " + outputFilename + " // " + listOfStringsToOneString(fileFormats))
 	listOfDirContent := getListOfDirAndFiles(startPath)
 
 	var resultList []string
 	for i := 0; i < len(listOfDirContent); i++ {
-		fmt.Println(getAbsoluteFilepath(listOfDirContent[i]))
-		if checkIsDir(getAbsoluteFilepath(listOfDirContent[i])) {
+		if checkIsDir(getAbsoluteFilepath(startPath, listOfDirContent[i])) {
 			recursiveWalkthrough(
-				getAbsoluteFilepath(listOfDirContent[i]+"\\"),
+				getAbsoluteFilepath(startPath, listOfDirContent[i]+"\\"),
 				outputFilename,
 				fileFormats)
 		} else {
 			if slices.Contains(fileFormats, getExtention(listOfDirContent[i])) {
-				resultList = append(resultList, getAbsoluteFilepath(listOfDirContent[i]))
+				resultList = append(resultList, getAbsoluteFilepath(startPath, listOfDirContent[i]))
 			}
 		}
 	}
 
-	myOutput := listOfStringsToOneString(resultList)
-	myOutputFile := filepath.Join(startPath, outputFilename)
-	//check(os.WriteFile(myOutputFile, []byte(myOutput), 0666))
-	fmt.Println(myOutputFile + " / " + myOutput + "\n")
+	if len(resultList) > 0 {
+		myOutput := getOutputXML(resultList)
+		myOutputFile := filepath.Join(startPath, outputFilename)
+		check(os.WriteFile(myOutputFile, []byte(myOutput), 0666))
+	}
 }
 
 func getExtention(name string) string {
-	fmt.Println("getExtention(" + name + ")\n")
 	result := filepath.Ext(name)
-	fmt.Println("Ext: " + result + "\n")
 	return result[1:]
 }
 
-/*
-func getOutput() string {
-	return listOfStringsToOneString(getListOfDirAndFiles(getStartDirPath()))
-}
-*/
-
 func getStartDirPath() string {
-	fmt.Println("getStartDirPath()")
-	return getDirOfArg(getAbsoluteFilepath(getMainStartupArg()))
+	return getDirOfArg(getMainStartupArg())
 }
 
-func getAbsoluteFilepath(s string) string {
-	fmt.Println("getAbsoluteFilepath(" + s + ")\n")
-	absoluteFilepath, err := filepath.Abs(s)
-	check(err)
-	return absoluteFilepath
+func getAbsoluteFilepath(parent string, s string) string {
+	if filepath.IsAbs(s) {
+		return s
+	} else {
+		return filepath.Join(parent, s)
+	}
 }
 
 func getMainStartupArg() string {
 	if len(os.Args) > 1 {
-		fmt.Println("Скормлена папка: " + os.Args[1])
+		//fmt.Println("Скормлена папка: " + os.Args[1])
 		return (os.Args[1])
 	} else {
-		fmt.Println("Вызван сам по себе, тестовая папка: \n" + "D:\\Prowler\\projects\\CNC-list-creator\\proj\\test")
-		// return os.Args[0]
-		return "D:\\Prowler\\projects\\CNC-list-creator\\proj\\test"
+		//fmt.Println("Вызван сам по себе: \n" + os.Args[0])
+		return os.Args[0]
 	}
 }
 
-func listOfStringsToOneString(strings []string) string {
-	fmt.Println("listOfStringsToOneString(" + strings + ")\n\n")
-	resString := ""
+/*
+func listOfStringsToOneString(stringsArg []string) string {
+		resString := ""
 
-	for i := 0; i < len(strings); i++ {
-		resString += strings[i] + "\n"
-	}
-	return resString
+		for i := 0; i < len(stringsArg); i++ {
+			resString += stringsArg[i] + "\n"
+		}
+		return resString
+	return strings.Join(stringsArg, "\n")
 }
+*/
 
 func getListOfDirAndFiles(givenFilename string) []string {
-	fmt.Println("getListOfDirAndFiles(" + givenFilename + ")\n")
 	var myList []string
 
-	if checkIsDir(getAbsoluteFilepath(givenFilename)) {
-
-		fmt.Println("--- " + getAbsoluteFilepath(givenFilename))
-		myFile, err1 := os.Open(getAbsoluteFilepath(givenFilename))
+	if checkIsDir(givenFilename) {
+		myFile, err1 := os.Open(givenFilename)
 		check(err1)
 
 		myList, err1 = myFile.Readdirnames(-1)
 		check(err1)
 	}
-
 	return myList
 }
 
 func getDirOfArg(givenFilename string) string {
-	fmt.Println("getDirOfArg(" + givenFilename + ")\n")
 	if checkIsDir(givenFilename) {
 		return givenFilename
 	} else {
@@ -145,7 +131,6 @@ func getDirOfArg(givenFilename string) string {
 }
 
 func checkIsDir(givenFilename string) bool {
-	fmt.Println("checkIsDir(" + givenFilename + ")\n")
 	myFileInfo, err1 := os.Stat(givenFilename)
 	check(err1)
 
@@ -161,4 +146,57 @@ func check(e error) {
 		log.Fatal(e)
 		panic(e)
 	}
+}
+
+func countDetails(detailCode string) string {
+	codeParts := strings.Split(detailCode, "_")
+	if len(codeParts) < 3 {
+		return ""
+	}
+	if codeParts[2] == "" {
+		return ""
+	}
+	for _, elem := range codeParts[2] {
+		if !unicode.IsDigit(elem) {
+			return ""
+		}
+	}
+	return codeParts[2]
+}
+
+func getOutputXML(myList []string) string {
+	resultString := "<WorkList><Version><Major>1</Major><Minor>0</Minor></Version><FileList>"
+	+getXMLFileList(myList)
+	+"</FileList><ProcessList>"
+	+getXMLProcessList(myList)
+	+"</ProcessList></WorkList>"
+
+	//	fmt.Println(resultString)
+
+	return resultString
+}
+
+func getXMLFileList(myList []string) string {
+	resString := "<Item><FileType>7</FileType><FilePath>"
+	resString += strings.Join(myList, "</FilePath></Item><Item><FileType>7</FileType><FilePath>")
+	resString += "</FilePath></Item>"
+	return resString
+}
+
+func getXMLProcessList(myList []string) string {
+	resString := ""
+	detailCode := ""
+	detailCount := ""
+	for _, elem := range myList {
+		detailCode = filepath.Base(elem)
+		detailCount = countDetails(detailCode)
+		if detailCount != "" {
+			resString += "<Item><SerialNum>"
+			+detailCode[:(len(detailCode) - 4)]
+			+"</SerialNum><PlanCount>"
+			+detailCount
+			+"</PlanCount><Count>0</Count></Item>"
+		}
+	}
+	return resString
 }
