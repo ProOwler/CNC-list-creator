ff50e332bc375930a8d9edd8dacac0e24e437025
ProOwler <proowler@gmail.com>
2025-02-17 23:56:31 +0700

    Скорректировал перезаписывание XML-файлов, 1 TODO
    Теперь файлы перезаписываются без хвоста и с табуляциями вместо пробелов
    
    TODO:
    - уточнить условия, при которых создаётся list.xml и редактируются файлы

--- main_test.go
+++ main_test.go
@@ -45,45 +45,45 @@ func TestGetUpdatedXML(t *testing.T) {
 	tThen := time.Now()
 	var testStrs = `<?xml version="1.0" encoding="utf-8" ?>
 <Root>
-  <Project Name="" Flag="SWJ008">
-    <Panels>
-      <Panel ID="1.4_4_Ковыряло_левое_(фрезеровка)" Name="740_432_1" Width="432.250" Length="740.500" Material="" Thickness="16.000" IsProduce="true" MachiningPoint="1" Type="1" Face5ID="" Face6ID="" Grain="L" Count="1">
-        <Machines>
-          <Machining ID="1000" Type="3" IsGenCode="2" Face="5" Depth="16.000" X="670.000" Y="25.000" Pocket="0" ToolOffset="左">
-            <Lines>
-              <Line LineID="1" EndX="715.000" EndY="25.000" Angle="0.000000" />
-              <Line LineID="2" EndX="725.000" EndY="35.000" Angle="-90.000" />
-            </Lines>
-          </Machining>
-        </Machines>
-        <EdgeGroup>
-          <Edge Face="1" Thickness="0.000000" />
-          <Edge Face="2" Thickness="0.000000" />
-        </EdgeGroup>
-      </Panel>
-    </Panels>
-  </Project>
+	<Project Name="" Flag="SWJ008">
+		<Panels>
+			<Panel ID="1.4_4_Ковыряло_левое_(фрезеровка)" Name="740_432_1" Width="432.250" Length="740.500" Material="" Thickness="16.000" IsProduce="true" MachiningPoint="1" Type="1" Face5ID="" Face6ID="" Grain="L" Count="1">
+				<Machines>
+					<Machining ID="1000" Type="3" IsGenCode="2" Face="5" Depth="16.000" X="670.000" Y="25.000" Pocket="0" ToolOffset="左">
+						<Lines>
+							<Line LineID="1" EndX="715.000" EndY="25.000" Angle="0.000000" />
+							<Line LineID="2" EndX="725.000" EndY="35.000" Angle="-90.000" />
+						</Lines>
+					</Machining>
+				</Machines>
+				<EdgeGroup>
+					<Edge Face="1" Thickness="0.000000" />
+					<Edge Face="2" Thickness="0.000000" />
+				</EdgeGroup>
+			</Panel>
+		</Panels>
+	</Project>
 </Root>`
 	var wantStrs = `<?xml version="1.0" encoding="utf-8" ?>
 <Root>
-  <Project Name="" Flag="SWJ008">
-    <Panels>
-      <Panel ID="1.4_4_Ковыряло_левое_(фрезеровка)" Name="740.5_432.2" Width="432.250" Length="740.500" Material="" Thickness="16.000" IsProduce="true" MachiningPoint="1" Type="1" Face5ID="" Face6ID="" Grain="L" Count="1">
-        <Machines>
-          <Machining ID="1000" Type="3" IsGenCode="2" Face="5" Depth="16.000" X="670.000" Y="25.000" Pocket="0" ToolOffset="左">
-            <Lines>
-              <Line LineID="1" EndX="715.000" EndY="25.000" Angle="0.000000" />
-              <Line LineID="2" EndX="725.000" EndY="35.000" Angle="-90.000" />
-            </Lines>
-          </Machining>
-        </Machines>
-        <EdgeGroup>
-          <Edge Face="1" Thickness="0.000000" />
-          <Edge Face="2" Thickness="0.000000" />
-        </EdgeGroup>
-      </Panel>
-    </Panels>
-  </Project>
+	<Project Name="" Flag="SWJ008">
+		<Panels>
+			<Panel ID="1.4_4_Ковыряло_левое_(фрезеровка)" Name="740.5_432.2" Width="432.250" Length="740.500" Material="" Thickness="16.000" IsProduce="true" MachiningPoint="1" Type="1" Face5ID="" Face6ID="" Grain="L" Count="1">
+				<Machines>
+					<Machining ID="1000" Type="3" IsGenCode="2" Face="5" Depth="16.000" X="670.000" Y="25.000" Pocket="0" ToolOffset="左">
+						<Lines>
+							<Line LineID="1" EndX="715.000" EndY="25.000" Angle="0.000000" />
+							<Line LineID="2" EndX="725.000" EndY="35.000" Angle="-90.000" />
+						</Lines>
+					</Machining>
+				</Machines>
+				<EdgeGroup>
+					<Edge Face="1" Thickness="0.000000" />
+					<Edge Face="2" Thickness="0.000000" />
+				</EdgeGroup>
+			</Panel>
+		</Panels>
+	</Project>
 </Root>`
 	// Action
 	got, err := getUpdatedXML(testStrs)
